250d01da9d2496cb1805a1622a9c2aa33c3b48e6
Brad Gravesen <bgravesen@gmail.com>
2024-05-17 06:47:29 -0700

    added dhcp-option-set-2

--- vpc.tf
+++ vpc.tf
@@ -8,3 +8,10 @@ import {
   id = "dopt-0b367bacc65f14a6d"
 }
 
+########################################################
+
+resource "aws_vpc_dhcp_options" "dhcp-option-set-2" {
+  domain_name_servers = ["8.8.8.8", "8.8.4.4"]
+  domain_name = "dev.bradgravesen.com"
+  ntp_servers = ["129.6.15.28"]
+}
